a36bf296773955ed835ab393fb543aec5430542e
Michael Krotscheck <krotscheck@gmail.com>
2016-04-26 17:20:38 -0500

    De-Montyfy Puppet-phabricator
    This patch takes what monty did, and cleans it up. Notable changes
    include:
    
    - Configuration has been extracted into a common config resource.
    - Installation components have been split into individual concerns,
      to allow some custom setups.
    - Directories are now all under /opt/phabricator
    - Database user has been given wildcard permissions on database
      phabricator%, to allow the database migration script to run.
    
    Change-Id: I41d808f68aa8587df797b8107dba043b7cccb1a6

---
+++ manifests/config.pp
@@ -0,0 +1,44 @@
+# Copyright 2016 Hewlett Packard Enterprise Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: phabricator
+#
+class phabricator::config (
+	# Database Configurations.
+	$mysql_database          = 'phabricator',
+	$mysql_host              = 'localhost',
+	$mysql_port              = 3306,
+	$mysql_user              = 'phabricator',
+	$mysql_user_password,
+	$mysql_root_password,
+
+	# Phabricator working directory
+	$phabricator_dir        = '/opt/phabricator',
+
+	# SSL Certificates.
+	$ssl_cert_file           = undef,
+	$ssl_cert_file_contents  = undef, # If left empty puppet will not create file.
+	$ssl_chain_file          = undef,
+	$ssl_chain_file_contents = undef, # If left empty puppet will not create file.
+	$ssl_key_file            = undef,
+	$ssl_key_file_contents   = undef, # If left empty puppet will not create file.
+
+	# Virtual host config.
+	$httpd_vhost             = $::fqdn,
+	$httpd_admin_email       = 'noc@openstack.org',
+) {
+
+	# Non-configurable-options (derived)
+	$httpd_docroot           = "${phabricator_dir}/phabricator/webroot"
+}
