86820917d313f2dbade3dbbe3047e2f9d075d391
Michael Krotscheck <krotscheck@gmail.com>
2016-04-26 17:20:38 -0500

    De-Montyfy Puppet-phabricator
    This patch takes what monty did, and cleans it up. Notable changes
    include:
    
    - Configuration has been extracted into a common config resource.
    - Installation components have been split into individual concerns,
      to allow some custom setups.
    - Directories are now all under /opt/phabricator
    - Database user has been given wildcard permissions on database
      phabricator%, to allow the database migration script to run.
    
    Change-Id: I41d808f68aa8587df797b8107dba043b7cccb1a6

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,174 +1,86 @@
+# Copyright 2016 Hewlett Packard Enterprise Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
 # == Class: phabricator
 #
+# Set up a full, standalone instance of phabricator.
+#
 class phabricator (
-  $mysql_user_password,
-  $instance                = 'dev',
+  # Database Configurations.
   $mysql_database          = 'phabricator',
   $mysql_host              = 'localhost',
   $mysql_port              = 3306,
   $mysql_user              = 'phabricator',
-  $phab_dir                = '/phabricator',
-  $ssl_cert_file           = "/etc/ssl/certs/${::fqdn}.pem",
+  $mysql_user_password,
+  $mysql_root_password,
+
+  # Phabricator working directory
+  $phabricator_dir         = '/opt/phabricator',
+
+  # SSL Certificates.
+  $ssl_cert_file           = undef,
   $ssl_cert_file_contents  = undef, # If left empty puppet will not create file.
   $ssl_chain_file          = undef,
   $ssl_chain_file_contents = undef, # If left empty puppet will not create file.
-  $ssl_key_file            = "/etc/ssl/private/${::fqdn}.key",
-  $ssl_key_file_contents   = undef,  # If left empty puppet will not create file.
-  $vhost_name              = $::fqdn,
+  $ssl_key_file            = undef,
+  $ssl_key_file_contents   = undef, # If left empty puppet will not create file.
+
+  # Httpd config.
+  $httpd_vhost             = $::fqdn,
+  $httpd_admin_email       = 'noc@openstack.org',
 ) {
 
-  $instances_dir = "${phab_dir}/instances"
-  $instance_dir = "${instances_dir}/${instance}"
+  # Set up the shared configuration.
+  class { 'phabricator::config':
+    mysql_database          => $mysql_database,
+    mysql_host              => $mysql_host,
+    mysql_port              => $mysql_port,
+    mysql_user              => $mysql_user,
+    mysql_user_password     => $mysql_user_password,
+    mysql_root_password     => $mysql_root_password,
+    phabricator_dir         => $phabricator_dir,
+    ssl_cert_file           => $ssl_cert_file,
+    ssl_cert_file_contents  => $ssl_cert_file_contents,
+    ssl_chain_file          => $ssl_chain_file,
+    ssl_chain_file_contents => $ssl_chain_file_contents,
+    ssl_key_file            => $ssl_key_file,
+    ssl_key_file_contents   => $ssl_key_file_contents,
+    httpd_vhost             => $httpd_vhost,
+    httpd_admin_email       => $httpd_admin_email,
 
-  $packages = [
-    'php5',
-    'php5-mysql',
-    'php5-gd',
-    'php5-dev',
-    'php5-curl',
-    'php-apc',
-    'php5-cli',
-    'python-pygmentize'
-  ]
-  package { $packages:
-    ensure => installed,
-  }
-
-  if !defined(Package['git']) {
-    package { 'git':
-      ensure => present
-    }
-  }
-
-  file { $phab_dir:
-    ensure => directory,
-  }
-  file { $instances_dir:
-    ensure => directory,
-  }
-  file { $instance_dir:
-    ensure => directory,
-  }
-
-  if $ssl_cert_file_contents != undef {
-    file { $ssl_cert_file:
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0640',
-      content => $ssl_cert_file_contents,
-      before  => Httpd::Vhost[$vhost_name],
-    }
-  }
-
-  if $ssl_key_file_contents != undef {
-    file { $ssl_key_file:
-      owner   => 'root',
-      group   => 'ssl-cert',
-      mode    => '0640',
-      content => $ssl_key_file_contents,
-      before  => Httpd::Vhost[$vhost_name],
-    }
-  }
-
-  if $ssl_chain_file_contents != undef {
-    file { $ssl_chain_file:
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0640',
-      content => $ssl_chain_file_contents,
-      before  => Httpd::Vhost[$vhost_name],
-    }
-  }
-
-  vcsrepo { "${instance_dir}/phabricator":
-    ensure   => latest,
-    provider => git,
-    source   => 'https://github.com/phacility/phabricator.git',
-    require  => [
-      File[$instance_dir],
-      Package['git'],
+    before                  => [
+      Class['Phabricator::Certificates'],
+      Class['Phabricator::Httpd'],
+      Class['Phabricator::Mysql'],
+      Class['Phabricator::Install'],
     ]
   }
 
-  vcsrepo { "${instance_dir}/arcanist":
-    ensure   => latest,
-    provider => git,
-    source   => 'https://github.com/phacility/arcanist.git',
-    require  => [
-      File[$instance_dir],
-      Package['git'],
+  include phabricator::certificates
+  include phabricator::mysql
+
+  class { '::phabricator::httpd':
+    require => [
+      Class['phabricator::install'],
+      Class['phabricator::mysql'],
+      Class['phabricator::certificates']
     ]
   }
 
-  vcsrepo { "${instance_dir}/libphutil":
-    ensure   => latest,
-    provider => git,
-    source   => 'https://github.com/phacility/libphutil.git',
-    require  => [
-      File[$instance_dir],
-      Package['git'],
+  class { '::phabricator::install':
+    require => [
+      Class['phabricator::mysql'],
     ]
   }
-
-  file { 'initial.db':
-    ensure => present,
-    path   => "${phab_dir}/initial.db",
-    source => 'puppet:///modules/phabricator/initial.db',
-  }
-
-  file {'local.json':
-    ensure  => present,
-    path    => "${instance_dir}/phabricator/conf/local/local.json",
-    content => template('phabricator/local.json.erb'),
-  }
-
-  file { '/etc/php5/mods-available/phabricator.ini':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    content => "; configuration for phabricator\n; priority=20\npost_max_size = 32M",
-
-  }
-
-  file { '/etc/php5/apache2/conf.d/20-phabricator.ini':
-    ensure => 'link',
-    target => '/etc/php5/mods-available/phabricator.ini',
-    notify => Service['httpd'],
-  }
-
-  exec { 'load-initial-db':
-    command     => "/usr/bin/mysql < ${phab_dir}/initial.db && ${instance_dir}/phabricator/bin/storage upgrade --force",
-    unless      => "${instance_dir}/phabricator/bin/storage status",
-    subscribe   => File['initial.db'],
-    refreshonly => true,
-    require     => [
-                    Vcsrepo["${instance_dir}/phabricator"],
-                    File['initial.db'],
-                    ]
-  }
-
-  exec { 'update-database':
-    command     => "${instance_dir}/phabricator/bin/storage upgrade --force",
-    refreshonly => true,
-    subscribe   => Vcsrepo["${instance_dir}/phabricator"],
-    require     => Vcsrepo["${instance_dir}/phabricator"],
-  }
-
-  include ::httpd
-  include ::httpd::ssl
-  include ::httpd::php
-
-  httpd_mod { 'rewrite':
-    ensure => present,
-  }
-
-  ::httpd::vhost { $vhost_name:
-    port     => 443,
-    docroot  => "${instance_dir}/phabricator/webroot/",
-    priority => '50',
-    template => 'phabricator/vhost.erb',
-    ssl      => true,
-    require  => File[$instance_dir],
-  }
-
 }
