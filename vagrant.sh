80e2d8d0015c9c0f85bd8af938baddceeaaef1f7
Michael Krotscheck <krotscheck@gmail.com>
2016-04-26 17:21:03 -0500

    [WIP] Development tools for puppet-phabricator
    Vagrant based development tools, so that we can rapidly set up
    and test the puppet module.
    
    Change-Id: I80fc43516f5378e795a16c5919f4915aa5190334

---
+++ vagrant.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Install Puppet!
+if [ ! -f /etc/apt/sources.list.d/puppetlabs.list ]; then
+  lsbdistcodename=`lsb_release -c -s`
+  wget https://apt.puppetlabs.com/puppetlabs-release-${lsbdistcodename}.deb
+  sudo dpkg -i puppetlabs-release-${lsbdistcodename}.deb
+  sudo apt-get update
+  sudo apt-get dist-upgrade -y
+fi
+
+apt-get install git -y
+
+if [ ! -d /etc/puppet/modules/httpd ]; then
+   git clone git://git.openstack.org/openstack-infra/puppet-httpd /etc/puppet/modules/httpd
+fi
+if [ ! -d /etc/puppet/modules/firewall ]; then
+  puppet module install puppetlabs-firewall --version 1.1.3
+fi
+if [ ! -d /etc/puppet/modules/mysql ]; then
+  puppet module install puppetlabs-mysql --version 3.6.2
+fi
+#if [ ! -d /etc/puppet/modules/apache ]; then
+#  puppet module install puppetlabs-apache --version 0.0.4
+#fi
+#if [ ! -d /etc/puppet/modules/rabbitmq ]; then
+#  puppet module install puppetlabs-rabbitmq --version 5.0.0
+#fi
+if [ ! -d /etc/puppet/modules/inifile ]; then
+  puppet module install puppetlabs-inifile --version 1.1.3
+fi
+if [ ! -d /etc/puppet/modules/vcsrepo ]; then
+  puppet module install openstackci-vcsrepo --version 0.0.8
+fi
+#if [ ! -d /etc/puppet/modules/python ]; then
+#  puppet module install stankevich-python --version 1.6.6
+#fi
+
+# Symlink the module
+if [ ! -d /etc/puppet/modules/phabricator ]; then
+  sudo ln -s /vagrant /etc/puppet/modules/phabricator
+fi
