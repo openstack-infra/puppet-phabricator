3c8303e1dea2e798aed1bd802049274d29bb669a
Craige McWhirter <craige@mcwhirter.io>
2016-07-15 10:23:49 +1000

    Patches Required to Deliver Pholio
    This set of patches delivers the changes required to install and
    configure an instance of Pholio for the UX team.
    
    There are addiitonal files to test the puppet module.
    
    Change-Id: I394da0070214af5ceadb3e236eca8a1b78562a72
    Implements: Spec Pholio

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,4 +1,4 @@
-# Copyright 2014 Hewlett-Packard Development Company, L.P.
+ Copyright 2016 Hewlett Packard Enterprise Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -14,31 +14,41 @@
 #
 # == Class: phabricator::mysql
 #
+# Set up a mysql host for phabricator.
+#
 class phabricator::mysql(
-  $mysql_root_password,
-  $mysql_bind_address = '127.0.0.1',
-  $mysql_port         = '3306'
-  ) {
+  $mysql_host              = $phabricator::vars::mysql_host,
+  $mysql_port              = $phabricator::vars::mysql_port,
+  $mysql_user              = $phabricator::vars::mysql_user,
+  $mysql_user_password     = $phabricator::vars::mysql_user_password,
+  $mysql_root_password     = $phabricator::vars::mysql_root_password,
+) {
 
-    class { '::mysql::server':
-      config_hash => {
-        'root_password'  => $mysql_root_password,
-        'default_engine' => 'InnoDB',
-        'bind_address'   => $mysql_bind_address,
-        'port'           => $mysql_port,
-        }
-    }
-
-    mysql::server::config { 'phab_config':
-      settings => {
-        'mysqld' => {
-          'max_allowed_packet'      => '32M',
-          'sql_mode'                => 'STRICT_ALL_TABLES',
-          'ft_stopword_file'        => '/phabricator/instances/dev/phabricator/resources/sql/stopwords.txt',
-          'ft_min_word_len'         => '3',
-          'ft_boolean_syntax'       => '\' |-><()~*:""&^\'',
-          'innodb_buffer_pool_size' => '1600M',
-        }
+  class { '::mysql::server':
+    root_password           => $mysql_root_password,
+    remove_default_accounts => true,
+    override_options        => {
+      mysqld => {
+        max_allowed_packet      => '32M',
+        sql_mode                => 'STRICT_ALL_TABLES',
+        ft_stopword_file        => '/opt/phabricator/phabricator/resources/sql/stopwords.txt',
+        ft_min_word_len         => 3,
+        ft_boolean_syntax       => '\' |-><()~*:""&^\'',
+        innodb_buffer_pool_size => '1600M',
       }
-    }
+    },
   }
+
+  mysql_user { "${mysql_user}@${mysql_host}":
+    provider      => 'mysql',
+    password_hash => mysql_password($mysql_user_password),
+  }
+
+  # Phabricator creates a mess of tables. This ensures that we don't have
+  # to create ACL's for all of them.
+  mysql_grant { "${mysql_user}@${mysql_host}/phabricator%.*":
+    privileges => ['ALL'],
+    table      => 'phabricator%.*',
+    user       => "${mysql_user}@${mysql_host}",
+  }
+}
---
+++ vagrant.pp
@@ -0,0 +1,9 @@
+node default {
+  class { '::phabricator':
+    httpd_vhost         => '192.168.99.23',
+    mysql_user_password => 'phabricator',
+    mysql_root_password => 'supersecret',
+    ssl_cert_file       => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+    ssl_key_file        => '/etc/ssl/private/ssl-cert-snakeoil.key',
+  }
+}
